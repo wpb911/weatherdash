c79d8c14a8f554aee59b7843a30cfeafa69e5fb1
Omega <wpb911@gmail.com>
2020-11-21 18:46:22 -0500

    updated script to support adding new city and displaying the data at the same time.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var lon;
 
 // Initial array of cities
 var cities = ["Laurens", "Columbia", "New York", "San Francisco", "Raleigh"];
-// var cityInput = "";
+
 
 function updateButtons() {
 
@@ -24,13 +24,13 @@ function updateButtons() {
   for (var i = 0; i < cities.length; i++) {
 
     // Then dynamicaly generating buttons for each movie in the array.
-    // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+
     var a = $("<button>");
     // Adding a class
     a.addClass("list-group-item list-group-item-action");
-    // Adding a data-attribute with a value of the movie at index i
+    // Adding a data-attribute with a value of the city at index i
     a.attr("data-name", cities[i]);
-    // Providing the button's text with a value of the movie at index i
+    // Providing the button's text with a value of the city at index i
     a.text(cities[i]);
     // Adding the button to the HTML
     $("#cities-view").append(a);
@@ -39,20 +39,118 @@ function updateButtons() {
 
 
 //Add a city to the cities array and store the array to local storage 
+//when the search button is clicked 
 $("#add-city").on("click", function(event) {
   event.preventDefault();
   
   var city = $("#city-input").val().trim();
-  cities.push(city);
+  console.log("City = " + city);
+  // 
+  cities.unshift(city);
+ 
 
+  // This is our API key
+  var APIKey = "f1ae5ec6cb6b9f1dfc10836e21d90fc2";
+
+  // Here we are building the URL we need to query the database
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
+  city + "&appid=" + APIKey;
+  console.log(queryURL);
+
+  // Here we run our AJAX call to the OpenWeatherMap API
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+  // We store all of the retrieved data inside of an object called "response"
+  .then(function(response) {
+
+        // display current weather content 
+    $(".city").html("<h1>" + response.name + "(" + moment().format('l') + ")" + "</h1>");
+    $(".wind").text("Wind Speed: " + response.wind.speed);
+    $(".humidity").text("Humidity: " + response.main.humidity);
+    
+    // Convert the temp to fahrenheit
+    var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+    // display temp as fahrenheit    
+    $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+
+    //store latitude and longitude for the UV call
+    lat = response.coord.lat;
+    lon = response.coord.lon;    
+        
+    //https://api.openweathermap.org/data/2.5/onecall?lat=30.489772&lon=-99.771335&exclude=hourly,minutely,alerts&units=imperial
+    var uvQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" +lat + "&lon=" + lon + "&exclude=hourly,minutely,alerts&units=imperial&appid=" + APIKey;
+    console.log(uvQueryURL);
+
+    $.ajax({
+      url: uvQueryURL,
+      method: "GET"
+    })
+      // We store all of the retrieved data inside of an object called "uvResponse"
+      .then(function(uvResponse) {
+
+        // Log the queryURL
+        //console.log(uvQueryURL);
+
+        // Log the resulting object
+        console.log(uvResponse);
+
+        // Transfer content to HTML
+        var toDay = moment().day()
+        var nextDay  = moment().day() + 1
+        console.log("toDay = " + toDay);
+        console.log("nextDay = " + nextDay );
+        var oneDay = moment.duration({'days' : 1});
+        var twoDay = moment.duration({'days' : 2});
+        var threeDay = moment.duration({'days' : 3});
+        var fourDay = moment.duration({'days' : 4});
+        var fiveDay = moment.duration({'days' : 5});
+        var md = moment();
+        
+        //UV Index
+        $(".uv").text("UV: " + uvResponse.current.uvi);
+        
+        //Daily forcasts from onecall API 
+        $(".day1date").text("Date: " + moment(md).add(oneDay).format('l'));
+        $(".day1temp").text("Temp F: " + uvResponse.daily[1].temp.day);
+        $(".day1humid").text("Humidity:  " + uvResponse.daily[1].humidity);
+        
+        $(".day2date").text("Date: " + moment(md).add(twoDay).format('l'));
+        $(".day2temp").text("Temp F: " + uvResponse.daily[2].temp.day);
+        $(".day2humid").text("Humidity:  " + uvResponse.daily[2].humidity);
+        
+        $(".day3date").text("Date: " + moment(md).add(threeDay).format('l'));
+        $(".day3temp").text("Temp F: " + uvResponse.daily[3].temp.day);
+        $(".day3humid").text("Humidity:  " + uvResponse.daily[3].humidity);
+        
+        $(".day4date").text("Date: " + moment(md).add(fourDay).format('l'));
+        $(".day4temp").text("Temp F: " + uvResponse.daily[4].temp.day);
+        $(".day4humid").text("Humidity:  " + uvResponse.daily[4].humidity);
+        
+        $(".day5date").text("Date: " + moment(md).add(fiveDay).format('l'));
+        $(".day5temp").text("Temp F: " + uvResponse.daily[5].temp.day);
+        $(".day5humid").text("Humidity:  " + uvResponse.daily[5].humidity);        
+    
+      });
+
+  });
+
+  // store the cities list to local storage 
   localStorage.setItem("weatherCities", JSON.stringify(cities));
+
   //update cities array 
   updateButtons();
+  
+  //clear input box 
+  $("#city-input").val("");
   
 
 });
 
-// This function display all the city weather data
+// This function display all the city weather data from the list of 
+//cities displayed 
 function displayCityInfo() {
   console.log("Running display city info function");
   // This line will grab the text from the input box
@@ -149,7 +247,7 @@ function displayCityInfo() {
         //UV Index
         $(".uv").text("UV: " + uvResponse.current.uvi);
         
-        //Dailey forcasts from onecall API 
+        //Daily forcasts from onecall API 
         $(".day1date").text("Date: " + moment(md).add(oneDay).format('l'));
         $(".day1temp").text("Temp F: " + uvResponse.daily[1].temp.day);
         $(".day1humid").text("Humidity:  " + uvResponse.daily[1].humidity);
@@ -182,5 +280,8 @@ function displayCityInfo() {
 // Calling the renderButtons function at least once to display the initial list of cities
 //renderButtons();
 $(document).on("click", ".list-group-item ", displayCityInfo);
+
+
+
 
 updateButtons();
